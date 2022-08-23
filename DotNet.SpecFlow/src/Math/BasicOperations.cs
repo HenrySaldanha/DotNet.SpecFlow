b411baad38504369739485d9afd4062cfc7107be
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-08-22 21:21:46 -0300

    feat: add math operations

---
+++ DotNet.SpecFlow/src/Math/BasicOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace Math;
+
+public class BasicOperations : IBasicOperations
+{
+    public long Sum(int a, int b) => a + b;
+    public long Subtract(int a, int b) => a - b;
+    public long Multiply(int a, int b) => a * b;
+    public double Divide(int a, int b) => (double)a / (double)b;
+}
---
+++ DotNet.SpecFlow/src/Math/IBasicOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace Math;
+
+public interface IBasicOperations
+{
+    double Divide(int a, int b);
+    long Multiply(int a, int b);
+    long Subtract(int a, int b);
+    long Sum(int a, int b);
+}
